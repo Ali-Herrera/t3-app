3ee59feb9749aa8f418bd288a02001ca67a80bf9
Ali H <alisonherrera02@gmail.com>
2024-02-08 15:06:34 -0500

    add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,14 @@ import { api } from "t3-starter-app/utils/api";
 
 import "t3-starter-app/styles/globals.css";
 
+import { ClerkProvider } from '@clerk/nextjs'
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+  <ClerkProvider {...pageProps}>
+    <Component {...pageProps} />
+  </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
